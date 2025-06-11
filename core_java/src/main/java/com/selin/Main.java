351b8623837bdfe716ac886b912473850e706431
PC <selinz.bolukbas@gmail.com>
2025-06-11 16:56:08 +0300

    JAVA TEMEL KONULAR VE UYGULAMALAR Main Metodu

---
+++ core_java/src/main/java/com/selin/Main.java
@@ -0,0 +1,47 @@
+package com.selin;
+
+
+import com.selin.Enum.Gun;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* JA-1 ENUM – Sabitleri Tanımla
+        Gun bugun= Gun.CUMA;
+
+        System.out.println("Bugün günlerden: "+bugun.name());
+         */
+
+        /* JA-2 ArrayList Kullanımı
+        ArrayList<String> sehirler = new ArrayList<>();
+
+        sehirler.add("Izmir");
+        sehirler.add("Ankara");
+        sehirler.add("Istanbul");
+
+        for (String sehir:sehirler){
+            System.out.println(sehir);
+        }
+         */
+
+        /* JA-3 HashSet Kullanımı
+        HashSet<String> meyveler=new HashSet<>();
+
+        meyveler.add("elma");
+        meyveler.add("muz");
+        meyveler.add("kiraz");
+        meyveler.add("elma");
+
+        for(String meyve:meyveler){
+            System.out.println(meyve);
+        }
+         */
+
+
+
+    }
+}
